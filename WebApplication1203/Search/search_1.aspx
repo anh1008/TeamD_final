26ef496f59d270edf5213d4f857d9de42c5afea7
morrischen0 <asd940437@gmail.com>
2022-12-09 23:47:22 +0800

    1209

---
+++ WebApplication1203/Search/search_1.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="search_1.aspx.cs" Inherits="WebApplication1203.Search.search_1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    &nbsp;</p>
+        Search_by_something :  
+    <asp:TextBox ID="Search_box" runat="server" Height="27px" Width="138px"></asp:TextBox>
+    <asp:Button ID="enter_button" runat="server" Text="Enter" Height="31px" Width="66px" />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    </p>
+</asp:Content>
+    
---
+++ WebApplication1203/Search/search_1.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1203.Search
+{
+
+
+    public partial class search_1
+    {
+
+        /// <summary>
+        /// Search_box 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Search_box;
+
+        /// <summary>
+        /// enter_button 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button enter_button;
+
+        /// <summary>
+        /// GridView1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// SqlDataSource1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+    }
+}
